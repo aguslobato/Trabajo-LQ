a841595006fffa9bd7252ae9ee0fba6b4b77af71
Agustin Lobato <85140053+aguslobato@users.noreply.github.com>
2025-05-30 14:51:09 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+function toggleDropdown(e) {
+    e.preventDefault();
+    const menu = e.target.closest('.dropdown').querySelector('.dropdown-menu');
+    menu.classList.toggle('show');
+  }
+
+  // Cierra si se hace clic fuera
+  document.addEventListener('click', function (e) {
+    const dropdowns = document.querySelectorAll('.dropdown-menu');
+    dropdowns.forEach(menu => {
+      if (!menu.contains(e.target) && !menu.previousElementSibling.contains(e.target)) {
+        menu.classList.remove('show');
+      }
+    });
+  });
+
+    const openPanel = document.getElementById("openPanel");
+    const closePanel = document.getElementById("closePanel");
+    const sidePanel = document.getElementById("sidePanel");
+    const overlay = document.getElementById("overlay");
+
+    openPanel.addEventListener("click", () => {
+      sidePanel.classList.add("open");
+      overlay.classList.add("active");
+    });
+
+    closePanel.addEventListener("click", () => {
+      sidePanel.classList.remove("open");
+      overlay.classList.remove("active");
+    });
+
+    overlay.addEventListener("click", () => {
+      sidePanel.classList.remove("open");
+      overlay.classList.remove("active");
+    });
+
+
+    const toggle = document.getElementById("menuToggle");
+    const menu = document.getElementById("menu");
+
+    toggle.addEventListener("click", () => {
+      menu.classList.toggle("active");
+    });
